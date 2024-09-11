a7e9f0fc25c8dfb31e474cbe2c8ef40c07f76265
Icaro Jordano <Icarojordano>
2024-09-11 02:40:27 -0300

    feat(Navbar): add section Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+import image from '../images/logo.svg'
+import { IoMenuOutline } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+
+export function Navbar() {
+    return(
+        <div className="Navbar">
+            <IoMenuOutline className="menu" />
+            <img src={image} alt="" />
+            <div className="add">
+                <CiHeart></CiHeart>
+                <IoCartOutline></IoCartOutline>
+            </div>
+        </div>
+    )
+}
