8eca832ee6c04fbbef772867f98ffe21e63e36fa
Icaro Jordano <Icarojordano>
2024-09-11 04:00:14 -0300

    feat(Home) add Home in page initial

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css"
+export function Home() {
+    return(
+        <div className="Home">
+            <div className="background">
+                <h1>GOING FAST</h1>
+                <h2>Introducing <br></br> The Wooden Puffa Clog in Suede</h2>
+                <img src="https://iili.io/dR1Eyiv.jpg" alt="" />
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga laudantium at rerum nulla dicta illo totam aliquam odit modi enim id quia.</p>
+            <button>GET IT NOW</button>
+        </div>
+    )
+}
