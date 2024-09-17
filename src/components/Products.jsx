d5406724c0fcb1a663153676f9ed8b05a0cb5a99
Icaro Jordano <Icarojordano>
2024-09-17 03:38:18 -0300

    feat(Products):add Button Load More

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,20 @@
 import { CardProducts } from "./CardProducts"
 import "./Products.css"
+// import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export function Products() {
     return(
         <section className="Products">
-            <CardProducts></CardProducts>
-            <CardProducts></CardProducts>
-            <CardProducts></CardProducts>
-            <CardProducts></CardProducts>
-            <CardProducts></CardProducts>
-            <CardProducts></CardProducts>
+            <div className="Cards">
+                <CardProducts></CardProducts>
+                <CardProducts></CardProducts>
+                <CardProducts></CardProducts>
+                <CardProducts></CardProducts>
+                <CardProducts></CardProducts>
+                <CardProducts></CardProducts>
+            </div>
+            <p className="viewed" >Viewed 72 of 745 products</p>
+            <button>LOAD MORE</button>
         </section>
     )
 }
