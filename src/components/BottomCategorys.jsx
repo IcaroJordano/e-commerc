41a9c538ab7f451d7c0eea6860ec34e2491270d4
Icaro Jordano <Icarojordano>
2024-09-18 02:07:27 -0300

    feat(bottom-categorys):scroll and animation

--- src/components/BottomCategorys.jsx
+++ src/components/BottomCategorys.jsx
@@ -2,17 +2,13 @@ import "./BottomCategorys.css"
 
 export function BottomCategorys() {
     return(
-        <section className="BottomCategorys">
+        <section className="logos">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
             <p>love</p>
             <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
-            <p>love</p>
+
         </section>
     )
 }
