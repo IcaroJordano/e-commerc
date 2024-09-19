e6e359867eff0d6bf7be7c1688372d2fa3b7ce46
Icaro Jordano <Icarojordano>
2024-09-19 00:42:15 -0300

    feat(Product):add multiples cards

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -8,7 +8,7 @@ function Produto() {
   useEffect(()=>{
     fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=10").then((res)=>res.json()).then((date)=>{
         console.log(date[3]) 
-      setProduct([date[1]])
+      setProduct([date[1],date[4]])
       console.log(product)
     //   console.log((product[0]).images)
     } 
@@ -19,7 +19,7 @@ function Produto() {
     <div className='content'>
         {/* <CardProducts price={1} text={1} imagem={1}></CardProducts> */}
         <CardProducts price={product[0].price} text={product[0].title} imagem={product[0].images}></CardProducts>
-        {/* <CardProducts price={product[1].price} text={product[1].title} imagem={product[1].images}></CardProducts> */}
+        <CardProducts price={product[1].price} text={product[1].title} imagem={product[1].images}></CardProducts>
         {/* <CardProducts price={product[1].price} text={product[1].title} imagem={product[1].images}></CardProducts>
         <CardProducts price={product[2].price} text={product[2].title} imagem={product[2].images}></CardProducts> */}
     </div>
