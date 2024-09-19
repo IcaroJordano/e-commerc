8ccf3a866f2c9c93ba3969936bf8107561bad589
Icaro Jordano <Icarojordano>
2024-09-19 00:37:04 -0300

    feat(Procudct):request api and expor in front

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { Categorys } from './components/Categorys'
 import { Home } from './components/Home'
 import { Navbar } from './components/Navbar'
 import { Products } from './components/Products'
+import Produto from './components/Produto'
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar>
       <Home></Home>
       <Categorys></Categorys>
       <Products></Products>
-      <BottomCategorys></BottomCategorys>
+      <BottomCategorys></BottomCategorys> */}
+      <Produto></Produto>
     </>
   )
 }
---
+++ src/components/Produto.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { CardProducts } from './CardProducts'
+import './Produto.css'
+
+function Produto() {
+  const [product,setProduct] = useState([])
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=10").then((res)=>res.json()).then((date)=>{
+        console.log(date[3]) 
+      setProduct([date[1]])
+      console.log(product)
+    //   console.log((product[0]).images)
+    } 
+      )
+    .catch((erro)=>console.log(erro))
+},[])
+  return (
+    <div className='content'>
+        {/* <CardProducts price={1} text={1} imagem={1}></CardProducts> */}
+        <CardProducts price={product[0].price} text={product[0].title} imagem={product[0].images}></CardProducts>
+        {/* <CardProducts price={product[1].price} text={product[1].title} imagem={product[1].images}></CardProducts> */}
+        {/* <CardProducts price={product[1].price} text={product[1].title} imagem={product[1].images}></CardProducts>
+        <CardProducts price={product[2].price} text={product[2].title} imagem={product[2].images}></CardProducts> */}
+    </div>
+  )
+}
+
+export default Produto
