41c55afc6331093af5e12db1a698c2f43791bd70
Icaro Jordano <Icarojordano>
2024-09-22 02:17:32 -0300

    feat(Categorys): request and render categorys

--- src/components/Categorys.jsx
+++ src/components/Categorys.jsx
@@ -1,4 +1,8 @@
 import "./Categorys.css"
+
+import { useState } from 'react'
+import { useEffect } from 'react'
+
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
@@ -11,6 +15,22 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 export function Categorys(){
+
+  const [product,setProduct] = useState([])
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/categories").then((res)=>res.json()).then((date)=>{
+        console.log(date) 
+        let list=[]
+        date.map((item)=>{
+          console.table(item.image)
+          list.push(item.image  )
+        })
+        setProduct(list)
+      }
+    
+      )
+    .catch((erro)=>console.log(erro))
+},[])
   return (
     <div className="Categorys">
 
@@ -26,10 +46,15 @@ export function Categorys(){
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide className='style slide'><div className="Card">1</div></SwiperSlide>
-      <SwiperSlide className='style slide'><div className="Card">2</div></SwiperSlide>
+      {
+        product.map((item)=>(
+          <SwiperSlide className='style slide' style={{'background-color':'red'}} ><div className="Card" style={{'background-image':`url(${item})`}} >1</div></SwiperSlide>
+
+        ))
+      }
+      {/* <SwiperSlide className='style slide'><div className="Card">2</div></SwiperSlide>
       <SwiperSlide className='style slide'><div className="Card">3</div></SwiperSlide>
-      <SwiperSlide className='style slide'><div className="Card">4</div></SwiperSlide>
+      <SwiperSlide className='style slide'><div className="Card">4</div></SwiperSlide> */}
 
     </Swiper>
     </div>
--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -6,13 +6,15 @@ import './Produto.css'
 function Produto() {
   const [product,setProduct] = useState([])
   useEffect(()=>{
-    fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=70").then((res)=>res.json()).then((date)=>{
-        console.log(date[2]) 
+    fetch("https://api.escuelajs.co/api/v1/categories").then((res)=>res.json()).then((date)=>{
+        console.log(date) 
         let list=[]
-        for (let i=0;i<=9;i++){
-          list.push(date[i])
-        }
-        setProduct(list)}
+        date.map((item)=>{
+          console.table(item.image)
+          list.push(item.image)
+        })
+        setProduct(list)
+      }
     
       )
     .catch((erro)=>console.log(erro))
@@ -20,7 +22,7 @@ function Produto() {
   return (
     <div className='content'>
       {product.map((item)=>(
-        <CardProducts price={item.price} text={item.title} imagem={item.images}></CardProducts>))
+        <CardProducts  imagem={item}></CardProducts>))
       }
       <p className="viewed" >Viewed 72 of 745 products</p>
       <button>LOAD MORE</button>  
